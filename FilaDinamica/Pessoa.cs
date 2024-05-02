688b3bbd4d6f7f86a5fdc73826d38edc030c3c7f
JoaoMazzoni <joao.mazzoni@5by5.com>
2024-05-02 13:39:09 -0300

    Add project files.

---
+++ FilaDinamica/Pessoa.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilaDinamica
+{
+    internal class Pessoa
+    {
+        string nome; // |?| - Modificador de tipo, indica que talvez, o atributo seja nulo em algum momento.
+        Pessoa? proximo; //Recebe a próxima pessoa da fila referente aquele objeto, por isso é do tipo "Pessoa". pessoa1.proximo = pessoa2 | pessoa2.proximo = pessoa3
+
+        public Pessoa(string nome)
+        {
+            this.nome = nome;
+            proximo = null;
+        }
+
+       
+
+        public void setNext(Pessoa aux) //Recebe uma pessoa no parâmetro
+        {
+            this.proximo = aux; // O próximo do tail da fila recebe a pessoa que estava no aux.
+        }
+
+        public Pessoa? getNext()
+        {
+            return this.proximo;
+        }
+
+        public override string? ToString()
+        {
+            return "\n\nNome da Pessoa: " + nome;
+        }
+
+
+    }
+}
---
+++ FilaDinamica/Program.cs
@@ -0,0 +1,28 @@
+﻿using FilaDinamica;
+
+internal class Program
+{ 
+
+    private static void Main(string[] args)
+    {
+
+        Console.WriteLine("Hello, Fila!");
+
+        FilaPessoa filaPessoa = new FilaPessoa();
+
+
+        filaPessoa.Push(new Pessoa("Odair")); //Cria um objeto da Classe Pessoa.
+        filaPessoa.Push(new Pessoa("Alexa"));
+        filaPessoa.Push(new Pessoa("João"));
+        filaPessoa.Print();
+        filaPessoa.Pop();
+        filaPessoa.Pop();
+        filaPessoa.Pop();
+        filaPessoa.Print();
+      
+    }
+
+
+
+
+}           
